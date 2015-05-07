bf8120ca2bd8a33e289ccf19a12bfba44c18aab6
gulu@wfc <gemfeeling@hotmail.com>
2015-05-07 12:20:09 +0800

    添加测试用的 unity 场景，在场景内唯一 Cube 上挂有一个测试脚本 TestLog.cs 里面包含了一组测试运行所有的 Log 输出项

---
+++ ulog_test/Assets/TestLog.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using ulog;
+using System;
+
+public class TestLog : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Log.LogInfo("test log info: {0} {1} {2}", 0, 3.5f, "foo");
+        Log.LogTODO("test warning.");
+        Log.LogWarning("test warning.");
+        Log.LogError("test error.");
+        Log.LogAssert(false, "test assert");
+        Log.LogException(new Exception());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
