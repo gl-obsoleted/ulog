ecd8bf59d1ddece616d491c89c10663124ad5b81
gulu@wfc <gemfeeling@hotmail.com>
2015-06-01 16:35:08 +0800

    去掉 GameCommon 命名空间

--- ulog/LogService.cs
+++ ulog/LogService.cs
@@ -6,150 +6,147 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 
-namespace GameCommon
+public class LogEventArgs : EventArgs
 {
-    public class LogEventArgs : EventArgs
+    public LogEventArgs(int seqID, LogType type, string content, float time)
     {
-        public LogEventArgs(int seqID, LogType type, string content, float time)
-        {
-            SeqID = seqID;
-            LogType = type;
-            Content = content;
-            Time = time;
-        }
-
-        public int SeqID = 0;
-        public LogType LogType;
-        public string Content = "";
-        public float Time = 0.0f;
+        SeqID = seqID;
+        LogType = type;
+        Content = content;
+        Time = time;
     }
 
-    public delegate void LogTargetHandler(object sender, LogEventArgs args);
+    public int SeqID = 0;
+    public LogType LogType;
+    public string Content = "";
+    public float Time = 0.0f;
+}
 
-    public class LogService : IDisposable
+public delegate void LogTargetHandler(object sender, LogEventArgs args);
+
+public class LogService : IDisposable
+{
+    public event LogTargetHandler LogTargets;
+
+    public LogService(bool logIntoFile)
     {
-        public event LogTargetHandler LogTargets;
+        RegisterCallback();
 
-        public LogService(bool logIntoFile)
+        if (logIntoFile)
         {
-            RegisterCallback();
-
-            if (logIntoFile)
-            {
-                try
-                {
-                    DateTime dt = DateTime.Now;
-
-                    string logDir = SysUtil.CombinePaths(Application.persistentDataPath, "log", SysUtil.FormatDateAsFileNameString(dt));
-                    Directory.CreateDirectory(logDir);
-
-                    string logPath = Path.Combine(logDir, SysUtil.FormatTimeAsFileNameString(dt) + ".txt");
-
-                    _logWriter = new FileInfo(logPath).CreateText();
-                    _logWriter.AutoFlush = true;
-                    _logPath = logPath;
-
-                    Log.Info("'Log Into File' enabled, file opened successfully. ('{0}')", _logPath);
-                }
-                catch (System.Exception ex)
-                {
-                    Log.Info("'Log Into File' enabled but failed to open file.");
-                    Log.Exception(ex);
-                }
-            }
-            else
-            {
-                Log.Info("'Log Into File' disabled.");
-            }
-        }
-
-        public void Dispose()
-        {
-            Log.Info("destroying log service...");
-
-            if (_logWriter != null)
-            {
-                _logWriter.Close();
-            }
-
-#if UNITY_5_0
-            Application.logMessageReceivedThreaded -= Logging.OnLogReceived;
-#endif
-
-            _disposed = true;
-        }
-
-        private void RegisterCallback()
-        {
-#if UNITY_5_0
-            Application.logMessageReceivedThreaded += Logging.OnLogReceived;
-#else
-            Application.RegisterLogCallbackThreaded(OnLogReceived);
-#endif
-        }
-
-        private void OnLogReceived(string condition, string stackTrace, LogType type)
-        {
-            if (_disposed || _isWriting)
-                return;
-
-            _isWriting = true;
-
-            ++_seqID;
-
-            switch (type)
-            {
-                case LogType.Assert:
-                    _assertCount++;
-                    break;
-                case LogType.Error:
-                    _errorCount++;
-                    break;
-                case LogType.Exception:
-                    _exceptionCount++;
-                    break;
-
-                case LogType.Warning:
-                case LogType.Log:
-                default:
-                    break;
-            }
-
             try
             {
-                if (_logWriter != null)
-                {
-                    _logWriter.WriteLine("{0:0.00} {1}: {2}", Time.realtimeSinceStartup, type, condition);
-                }
+                DateTime dt = DateTime.Now;
 
-                foreach (LogTargetHandler Caster in LogTargets.GetInvocationList())
-                {
-                    ISynchronizeInvoke SyncInvoke = Caster.Target as ISynchronizeInvoke;
-                    LogEventArgs args = new LogEventArgs(_seqID, type, condition, Time.realtimeSinceStartup);
+                string logDir = SysUtil.CombinePaths(Application.persistentDataPath, "log", SysUtil.FormatDateAsFileNameString(dt));
+                Directory.CreateDirectory(logDir);
 
-                    if (SyncInvoke != null && SyncInvoke.InvokeRequired)
-                        SyncInvoke.Invoke(Caster, new object[] { this, args });
-                    else
-                        Caster(this, args);
-                }
+                string logPath = Path.Combine(logDir, SysUtil.FormatTimeAsFileNameString(dt) + ".txt");
+
+                _logWriter = new FileInfo(logPath).CreateText();
+                _logWriter.AutoFlush = true;
+                _logPath = logPath;
+
+                Log.Info("'Log Into File' enabled, file opened successfully. ('{0}')", _logPath);
             }
             catch (System.Exception ex)
             {
-                Log.Exception(ex); // this should at least print to Unity Editor (but skip the file writing due to earlier writing failure)            	
+                Log.Info("'Log Into File' enabled but failed to open file.");
+                Log.Exception(ex);
             }
+        }
+        else
+        {
+            Log.Info("'Log Into File' disabled.");
+        }
+    }
 
-            _isWriting = false;
+    public void Dispose()
+    {
+        Log.Info("destroying log service...");
+
+        if (_logWriter != null)
+        {
+            _logWriter.Close();
         }
 
-        private string _logPath;
-        private StreamWriter _logWriter;
+#if UNITY_5_0
+        Application.logMessageReceivedThreaded -= Logging.OnLogReceived;
+#endif
 
-        private ushort _seqID = 0;
-        private int _assertCount = 0;
-        private int _errorCount = 0;
-        private int _exceptionCount = 0;
-
-        private bool _disposed = false;
-        private bool _isWriting = false;
+        _disposed = true;
     }
+
+    private void RegisterCallback()
+    {
+#if UNITY_5_0
+        Application.logMessageReceivedThreaded += Logging.OnLogReceived;
+#else
+        Application.RegisterLogCallbackThreaded(OnLogReceived);
+#endif
+    }
+
+    private void OnLogReceived(string condition, string stackTrace, LogType type)
+    {
+        if (_disposed || _isWriting)
+            return;
+
+        _isWriting = true;
+
+        ++_seqID;
+
+        switch (type)
+        {
+            case LogType.Assert:
+                _assertCount++;
+                break;
+            case LogType.Error:
+                _errorCount++;
+                break;
+            case LogType.Exception:
+                _exceptionCount++;
+                break;
+
+            case LogType.Warning:
+            case LogType.Log:
+            default:
+                break;
+        }
+
+        try
+        {
+            if (_logWriter != null)
+            {
+                _logWriter.WriteLine("{0:0.00} {1}: {2}", Time.realtimeSinceStartup, type, condition);
+            }
+
+            foreach (LogTargetHandler Caster in LogTargets.GetInvocationList())
+            {
+                ISynchronizeInvoke SyncInvoke = Caster.Target as ISynchronizeInvoke;
+                LogEventArgs args = new LogEventArgs(_seqID, type, condition, Time.realtimeSinceStartup);
+
+                if (SyncInvoke != null && SyncInvoke.InvokeRequired)
+                    SyncInvoke.Invoke(Caster, new object[] { this, args });
+                else
+                    Caster(this, args);
+            }
+        }
+        catch (System.Exception ex)
+        {
+            Log.Exception(ex); // this should at least print to Unity Editor (but skip the file writing due to earlier writing failure)            	
+        }
+
+        _isWriting = false;
+    }
+
+    private string _logPath;
+    private StreamWriter _logWriter;
+
+    private ushort _seqID = 0;
+    private int _assertCount = 0;
+    private int _errorCount = 0;
+    private int _exceptionCount = 0;
+
+    private bool _disposed = false;
+    private bool _isWriting = false;
 }
--- ulog/SysUtil.cs
+++ ulog/SysUtil.cs
@@ -4,28 +4,25 @@ using System.IO;
 using System.Linq;
 using System.Text;
 
-namespace GameCommon
+public class SysUtil
 {
-    public class SysUtil
+    public static string CombinePaths(params string[] paths)
     {
-        public static string CombinePaths(params string[] paths)
+        if (paths == null)
         {
-            if (paths == null)
-            {
-                throw new ArgumentNullException("paths");
-            }
-
-            return paths.Aggregate(Path.Combine);
+            throw new ArgumentNullException("paths");
         }
 
-        public static string FormatDateAsFileNameString(DateTime dt)
-        {
-            return string.Format("{0:0000}-{1:00}-{2:00}", dt.Year, dt.Month, dt.Day);
-        }
+        return paths.Aggregate(Path.Combine);
+    }
 
-        public static string FormatTimeAsFileNameString(DateTime dt)
-        {
-            return string.Format("{0:00}-{1:00}-{2:00}", dt.Hour, dt.Minute, dt.Second);
-        }
+    public static string FormatDateAsFileNameString(DateTime dt)
+    {
+        return string.Format("{0:0000}-{1:00}-{2:00}", dt.Year, dt.Month, dt.Day);
+    }
+
+    public static string FormatTimeAsFileNameString(DateTime dt)
+    {
+        return string.Format("{0:00}-{1:00}-{2:00}", dt.Hour, dt.Minute, dt.Second);
     }
 }
